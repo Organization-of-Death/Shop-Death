0d913f0136edc15eb13c8a4aa97cd81fa5f7ffec
hacchaa <jomwit22@gmail.com>
2022-11-16 00:55:11 +0700

    fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_160532) do
-  create_table "inventories", force: :cascade do |t|
-    t.decimal "price"
-    t.integer "qty"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "buyer_id"
-    t.integer "seller_id"
-    t.integer "item_id"
-    t.index ["buyer_id"], name: "index_inventories_on_buyer_id"
-    t.index ["item_id"], name: "index_inventories_on_item_id"
-    t.index ["seller_id"], name: "index_inventories_on_seller_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_175145) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -46,14 +33,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_160532) do
     t.string "name"
     t.string "username"
     t.string "password"
-    t.integer "user_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_type"
   end
 
-  add_foreign_key "inventories", "Users", column: "buyer_id"
-  add_foreign_key "inventories", "Users", column: "seller_id"
-  add_foreign_key "inventories", "items"
   add_foreign_key "markets", "Users", column: "seller_id"
   add_foreign_key "markets", "items"
 end
