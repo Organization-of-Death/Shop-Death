3fa63b5fbeb00e8447c065fa6d5dd8acae699220
TheresaRoss <thutgreybear@gmail.com>
2022-11-18 19:46:31 +0700

    finished /inventory part, ready to merge

--- app/controllers/myinven_controller.rb
+++ app/controllers/myinven_controller.rb
@@ -5,11 +5,13 @@ class MyinvenController < ApplicationController
     Item.connection
     @all_market = Market.all
     @size =@all_market.length-1
+    Inventory.connection
    
    
     
   end
   def buy1
+
     puts 'saaaaaaa'
     asize =(params[:size].to_i)
     jesus = false
@@ -21,9 +23,21 @@ class MyinvenController < ApplicationController
         market = Market.find_by id: params["market_id#{i}"]
         market.stock -= params["amount#{i}"].to_i
         market.save
+        inven = Inventory.new
+        market = Market.find_by id: params["market_id#{i}"]
+        inven.item_id =  market.item_id
+        inven.price = market.price
+        inven.qty = params["amount#{i}"].to_i
+        iid =User.find_by username:session[:username]
+        inven.buyer_id = iid.id
+        inven.seller_id = market.seller_id
+        inven.save
+
       end
     end
     if jesus
+      puts 'saaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+      puts Inventory.all
       redirect_to myinven_main_path, notice:'Buy successfully!' and return
     else
       redirect_to myinven_main_path, notice: 'Buy something first' and return
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    #has_many :Inventory,  :dependent => :destroy
+    has_many :Inventory,  foreign_key: :seller_id,  foreign_key: :buyer_id,  :dependent => :destroy
     has_many :Market,  foreign_key: :seller_id,  :dependent => :destroy
 end
